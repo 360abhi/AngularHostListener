bc58f95f7f811457aa02ce2e30f9b6f2081dc0cf
Abhishek Chhawari <abhishekch360@gmail.com>
2023-02-11 00:44:09 +0530

    child content renderer

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent  {
 
+  @ContentChild('childcont') childparagraph : ElementRef;
+
+  constructor(private renderer: Renderer2){}
+
+  addText() : void {
+    let text = this.renderer.createText(" this is the added text");
+    this.renderer.appendChild(this.childparagraph.nativeElement,text);
+  }
+ 
+
+ 
+  
 }
