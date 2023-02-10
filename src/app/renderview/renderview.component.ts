af3bc2c617e3a8442cfa6d1a688cfbe072182493
Abhishek Chhawari <abhishekch360@gmail.com>
2023-02-11 00:29:06 +0530

    viewchild and renderer

--- src/app/renderview/renderview.component.ts
+++ src/app/renderview/renderview.component.ts
@@ -13,20 +13,42 @@ import {
 })
 export class RenderviewComponent implements AfterViewInit {
   @ViewChild('box') box: ElementRef;
+  @ViewChild('para') para : ElementRef;
+
   toggle: boolean = false;
+  toggle2: boolean = false;
 
   constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {}
 
+  
   changeColor(): void {
     this.toggle = !this.toggle;
     if (this.toggle) {
       this.renderer.setStyle(this.box.nativeElement, 'backgroundColor', 'blue');
+      // this.renderer.setStyle(this.box.nativeElement, 'color', 'white');
       // this.box.nativeElement.style.backgroundColor = 'blue'
     }else{
       this.renderer.setStyle(this.box.nativeElement, 'backgroundColor', 'red');
+      // this.renderer.setStyle(this.box.nativeElement, 'color', 'black');
       // this.box.nativeElement.style.backgroundColor = 'red'
     }
   }
+  changeText() : void {
+    this.toggle2 = !this.toggle2;
+    if (this.toggle2) {
+      this.renderer.setProperty(this.para.nativeElement,'innerHTML',"the content is changed")
+      // this.renderer.setStyle(this.box.nativeElement, 'color', 'white');
+      // this.box.nativeElement.style.backgroundColor = 'blue'
+    }else{
+      this.renderer.setProperty(this.para.nativeElement,'innerHTML',"View Page of Styling App")
+      // this.renderer.setStyle(this.box.nativeElement, 'color', 'black');
+      // this.box.nativeElement.style.backgroundColor = 'red'
+    }
+   
+    // console.log(this.para)
+  //   this.para.nativeElement.style.innerHTML = "changed"
+  // }
+}
 }
