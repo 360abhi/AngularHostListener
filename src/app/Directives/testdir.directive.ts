7c04ed536c9e1a7ec970a8f13aa7cf363474aad5
Abhishek Chhawari <abhishekch360@gmail.com>
2023-02-11 01:00:59 +0530

    directive and renderer

---
+++ src/app/Directives/testdir.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appTestdir]'
+})
+export class TestdirDirective {
+
+  constructor(private el: ElementRef,private renderer: Renderer2) {
+    // this.renderer.setStyle(this.el.nativeElement,'backgroundColor','blue')
+   }
+
+   changeColor(color:string): void {
+    this.renderer.setStyle(this.el.nativeElement,'backgroundColor',color)
+   }
+
+}
